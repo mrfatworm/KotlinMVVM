885d2564134f7023a84268d24bab3abe00194f7d
mrfatworm <mrfatworm@gmail.com>
2021-03-28 15:12:11 +0800

    Basic BMI calculation

--- app/src/main/java/com/mrfatworm/kotlinmvvm/MainActivity.kt
+++ app/src/main/java/com/mrfatworm/kotlinmvvm/MainActivity.kt
@@ -2,23 +2,46 @@ package com.mrfatworm.kotlinmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.mrfatworm.kotlinmvvm.databinding.ActivityMainBinding
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity(), IMvp {
 
-    private lateinit var tvMVP: TextView
-
     private var presenter = MainActivityPresenter(this)
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        tvMVP = findViewById(R.id.tv_mvp)
         presenter.requestUserName()
     }
 
     override fun receiveName(name: String) {
-        tvMVP.text = name
+        binding.tvWeight.text = name
+    }
+
+    fun bmi(view: View) {
+        try {
+            val weight = binding.edtWeight.text.toString().toFloat()
+            val height = binding.edtHeight.text.toString().toFloat()
+            val bmi = weight / (height * height)
+            Toast.makeText(this, "BMI = $bmi", Toast.LENGTH_SHORT).show()
+        }catch (e: Exception){
+            Toast.makeText(this, "You must input value", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun help(view: View) {
+        AlertDialog.Builder(this)
+                .setMessage("Input your height(meter) and weight")
+                .setTitle("Hint")
+                .setPositiveButton("OK", null)
+                .show()
     }
 }
